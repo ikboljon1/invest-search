5dd7afad46a851f6f52aa7a940e80c09d0f85846
Ikboljon <63257726+ikboljon1@users.noreply.github.com>
2022-05-29 19:57:45 +0600

    invest-search

--- addproject/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Addproject, Partner,Price,Region,Category
-# Register your models here.
-admin.site.register(Addproject)
-admin.site.register(Partner)
-admin.site.register(Category)
-admin.site.register(Price)
-admin.site.register(Region)
